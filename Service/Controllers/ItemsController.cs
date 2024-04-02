50ae9e52ad43bf4af0e8f2916977620c5154b2bd
Alexey Ivanov <alex-iv999.ivanov@yandex.ru>
2024-04-02 19:28:55 +0300

    Added ItemsController.cs

---
+++ Service/Controllers/ItemsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static CatalogService.Dtos;
+
+namespace CatalogService.Controllers
+{
+
+    [ApiController]
+    [Route("api/items")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new List<ItemDto>()
+        {
+            new ItemDto(Guid.NewGuid(), "Potion", "Restore a HP",5, DateTimeOffset.Now),
+            new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison",10, DateTimeOffset.Now),
+            new ItemDto(Guid.NewGuid(), "Bronze sword", "Deals damage",25, DateTimeOffset.Now),
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get() => items;
+
+        [HttpGet("{id}")]
+        public ItemDto Get(int id) => items.FirstOrDefault(i => i.Equals(id));
+    }
+}
---
+++ Service/Dtos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CatalogService
+{
+    public class Dtos
+    {
+        public record ItemDto(Guid id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+        public record CreateItemDto(string Name, string Description, decimal Price);
+        public record UpdateItemDto(string Name, string Description, decimal Price);
+    }
+}
